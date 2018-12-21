529514bd2cd8c2e5dd9b68a2d8786a55b804df54
Abdullahi Yusuf <45642669+lovowAb@users.noreply.github.com>
2018-12-22 01:31:10 +0200

    Update my_phone.py

--- my_phone.py
+++ my_phone.py
@@ -6,5 +6,5 @@ import re     # regular expressions
 my_phone = "+20-115-926-3089" # Your Phone number
 number = re.sub(r"#.*$, " ", my_phone)
 print ("Your Phone number is: ", number)
-number = re.sub(r"\D", " ", my_phone)
+number = re.sub(r"\D", " ", my_phone)      # Removes all charecters 
 print ("Your Phone number without character is: ", number) 
