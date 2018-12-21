edaa136e12f7cb81efc5fc69fb7d8a6f4970706d
Abdullahi Yusuf <45642669+lovowAb@users.noreply.github.com>
2018-12-22 01:27:24 +0200

    Source code

--- my_phone.py
+++ my_phone.py
@@ -1,1 +1,10 @@
-
+"""
+    Abdullahi
+    lovowAb
+"""
+import re     # regular expressions
+my_phone = "+20-115-926-3089" # Your Phone number
+number = re.sub(r"#.*$, " ", my_phone)
+print ("Your Phone number is: ", number)
+number = re.sub(r"\D", " ", my_phone)
+print ("Your Phone number without character is: ", number) 
